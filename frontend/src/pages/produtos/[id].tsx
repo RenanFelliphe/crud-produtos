342630a564219386146fe204c0905f1bbcd986e1
Daniel Ventura de Almeida <danielvalmeida91@gmail.com>
2026-07-08 22:16:56 -0300

    feat: paginas de produtos e visualizacao do produto

--- frontend/src/pages/produtos/[id].tsx
+++ frontend/src/pages/produtos/[id].tsx
@@ -1,4 +1,5 @@
 import { api } from "@/api/axiosInstance";
+import { AxiosError } from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -7,23 +8,76 @@ import { useEffect, useState } from "react";
 export default function EditarProduto() {
   const router = useRouter();
   const { id } = router.query;
+  const [produto, setProduto] = useState(null);
+  const [nome, setNome] = useState("1");
+  const [descricao, setDescricao] = useState("");
+  const [quantidade, setQuantidade] = useState("");
+  const [preco, setPreco] = useState("");
 
   // TODO: um useState para cada campo do produto (nome, descricao, preco, quantidade)
-
   useEffect(() => {
     // TODO: buscar o produto (GET /produtos/:id) e preencher o formulário
+    if (id) {
+      const getProduct = async () => {
+        try {
+          const response = await api.get(`/produtos/${id}`)
+          if (response.data.data.length > 0) {
+            setProduto(response.data.data[0])
+            setNome(response.data.data[0].nome)
+            setDescricao(response.data.data[0].descricao)
+            setPreco(response.data.data[0].preco)
+            setQuantidade(response.data.data[0].quantidade)
+          }
+        } catch (error) {
+          if (error instanceof AxiosError) return console.warn(error?.response?.data)
+
+          console.log(error)
+        }
+      }
+      getProduct()
+    }
   }, [id]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // TODO: PUT /produtos/:id com os dados atualizados
+
+    const body = {
+      nome,
+      descricao,
+      quantidade: Number(quantidade),
+      preco: Number(preco)
+    }
+    try {
+      const updateProduct = await api.put(`/produtos/${id}`, body)
+      console.log('update', updateProduct.data)
+      router.push('/produtos')
+    } catch (error) {
+      if (error instanceof AxiosError) return console.warn(error?.response?.data)
+    }
     // TODO: redirecionar para /produtos após sucesso
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* TODO: inputs controlados para nome, descricao, preco e quantidade */}
-      {/* TODO: botão de submit */}
-    </form>
+    <>
+      {produto && (
+        <form onSubmit={handleSubmit} className="w-full h-screen flex flex-col items-center justify-center">
+          {/* TODO: inputs controlados para nome, descricao, preco e quantidade */}
+          <label htmlFor="">Nome do produto: </label>
+          <input className="px-1 py-2 min-w-24 border border-zinc-400 rounded-md mb-5" type="text" name="nome" onChange={(e) => setNome(e.target.value)} value={nome} />
+
+          <label htmlFor="">Descrição do produto: </label>
+          <input className="px-1 py-2 min-w-24 border border-zinc-400 rounded-md mb-5" type="text" name="descricao" onChange={(e) => setDescricao(e.target.value)} value={descricao} />
+
+          <label htmlFor="">Quantidade do produto: </label>
+          <input className="px-1 py-2 min-w-24 border border-zinc-400 rounded-md mb-5" type="text" name="quantidade" onChange={(e) => setQuantidade(e.target.value)} value={quantidade} />
+
+          <label htmlFor="">Preço do produto: </label>
+          <input className="px-1 py-2 min-w-24 border border-zinc-400 rounded-md mb-5" type="text" name="preco" onChange={(e) => setPreco(e.target.value)} value={preco} />
+          {/* TODO: botão de submit */}
+          <button className="p-4 min-w-18 border border-zinc-300 bg-sky-700 text-white rounded-sm">Atualizar</button>
+        </form>
+      )}
+    </>
   );
 }
--- frontend/src/pages/produtos/index.tsx
+++ frontend/src/pages/produtos/index.tsx
@@ -1,16 +1,68 @@
 import { api } from "@/api/axiosInstance";
+import { AxiosError } from "axios";
+import { Eye, Pencil, Trash } from "lucide-react";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Produtos() {
+  const router = useRouter()
   // TODO: estado para guardar a lista de produtos vinda da API
+  const [produtos, setProdutos] = useState([])
 
   useEffect(() => {
     // TODO: buscar os produtos (GET /produtos) ao carregar a página
+    const getProducts = async () => {
+      try {
+        const response = await api.get('/produtos')
+        setProdutos(response.data.data)
+      } catch (error) {
+        if (error instanceof AxiosError) return console.warn(error?.response?.data)
+
+        console.log(error)
+      }
+    }
+    getProducts()
   }, []);
+
+  const handleGetProduct = (productId: string) => { router.push(`/produtos/${productId}`) }
+  const handleEditProduct = () => { }
+  const handleDeleteProduct = () => { }
 
   return (
     <>
       {/* TODO: link para a página de cadastro (/produtos/novo) */}
+      <div className="w-full h-screen flex items-center justify-center">
+        <table>
+          <thead className="p-2">
+            <tr>
+              <th>Id</th>
+              <th>Nome</th>
+              <th>Descrição</th>
+              <th>Quantidade</th>
+              <th>Preço</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {produtos.map(produto => (
+              <tr key={produto.id}>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.descricao}</td>
+                <td>{produto.quantidade}</td>
+                <td>{produto.preco}</td>
+                <td>
+                  <div className="flex gap-2">
+                    <button onClick={() => handleGetProduct(produto.id)}><Eye className="text-zinc-400 hover:text-zinc-100" /></button>
+                    <button onClick={handleEditProduct}><Pencil className="text-zinc-400 hover:text-zinc-100" /></button>
+                    <button onClick={handleDeleteProduct}><Trash className="text-zinc-400 hover:text-zinc-100" /></button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
       {/* TODO: tabela listando os produtos (nome, preco, quantidade) */}
       {/* TODO: botão Editar -> leva para /produtos/[id] */}
